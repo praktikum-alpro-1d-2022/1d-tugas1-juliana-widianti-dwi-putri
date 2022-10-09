c8bbe6255af51145e81a5aa7b31c2db0c05da3a0
JWDP <114941525+JWDP@users.noreply.github.com>
2022-10-09 21:48:16 +0700

    Add files via upload

---
+++ Tugas Praktikum 1(1.identitass).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string npm = "2210631170025";
+    string namalengkap = "Juliana Widianti Dwi Putri";
+    int umur = 18;
+    string kelas = "1D";
+    string hobi = "Membaca Novel";
+    string cita = "Bekerja di Google Office";
+    cout << "NPM            :" << npm << endl;
+    cout << "Nama Lengkap   :" << namalengkap << endl;
+    cout << "Umur           :" << umur << endl;
+    cout << "Kelas          :" << kelas << endl;
+    cout << "Hobi           :" << hobi << endl;
+    cout << "Cita           :" << cita << endl;
+    return 0;
+}
